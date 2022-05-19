2748b36af2608247e4102ffaffe07d57811bd883
Anibal Andrade <90876706+Andradea3@users.noreply.github.com>
2022-05-19 13:42:34 -0600

    Initialize dependabot/npm_and_yarn/hosted-git-info-2.8.9

---
+++ web/app/themes/custom/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+error_reporting(E_ERROR | E_PARSE);
+
+require_once('inc/init.php');
+require_once('inc/class.utils.php');
+require_once('inc/assets.php');
+require_once('inc/gutenberg.php');
+require_once('inc/ajax-query.php');
